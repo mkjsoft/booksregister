54bba45749a10a2effeec150f62d340df2c26995
mjaworski <mkjsoft@gmail.com>
2021-03-11 10:15:25 +0100

    Config, Frontend

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,11 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/global.scss';
+
+const $ = require('jquery');
+
+require('bootstrap');
 
 // start the Stimulus application
 import './bootstrap';
